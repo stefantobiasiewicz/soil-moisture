49ae722a35d346b9e1a26c251b602c84ed427fe5
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-06-30 15:10:28 +0200

    hardware adc implementation

--- src/main.c
+++ src/main.c
@@ -6,7 +6,23 @@
 
 #include <zephyr/kernel.h>
 
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/adc.h>
+
+#include "hardware.h"
+
+
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	hardware_init();
+
+
+	
+	while(1) {
+		printk("soil moisture = %"PRId32" mV\n", hardware_read_adc_mv_moisture());
+		printk("battery = %"PRId32" mV\n", hardware_read_adc_mv_battery());
+
+		k_sleep(K_MSEC(1000));
+	}
 }
